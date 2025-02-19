8fa3f527f99a00328a61270d143f3ec78fa71c5a
Nikita Romanov <github@horodep.me>
2025-02-19 18:37:22 +0400

    Rebranding FileFormat.HEIC to Openize.HEIC

---
+++ Openize.IsoBmff/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Openize.IsoBmff
+ * Copyright (c) 2024-2025 Openize Pty Ltd. 
+ *
+ * This file is part of Openize.IsoBmff.
+ *
+ * Openize.IsoBmff is available under MIT license, which is
+ * available along with Openize.IsoBmff sources.
+ */
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Openize.IsoBmff")]
+[assembly: AssemblyProduct("Openize.IsoBmff")]
+[assembly: AssemblyDescription("Openize.IsoBmff is an open source implementation of the ISO/IEC 14496-12:2015 ISO base media file format.")]
+[assembly: AssemblyCompany("Openize Pty Ltd.")]
+[assembly: AssemblyCopyright("Copyright © 2024-2025 Openize Pty Ltd.")]
+
+[assembly: AssemblyVersion("25.2.0")]
+[assembly: AssemblyFileVersion("25.2.0")]
